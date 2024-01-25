ceccde67aadd1563eafc8d899d0ba388e9eddf7a
abammeke <abammeke222@gmail.com>
2024-01-25 16:32:09 -0600

    [S1003] - Uploaded my portfolio proj

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import App from "./components/App";
+import Jokes from "./components/Jokes";
+import Header from "./components/Header";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDom.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    /*  <React.StrictMode>*/
+    <BrowserRouter history={createBrowserHistory()}>
+        <Routes>
+            <Route path="/" element={<Header><App/></Header>} />
+            <Route path="/jokes" element={<Header><Jokes/></Header>} />
+        </Routes>
+    </BrowserRouter>
+    /*  </React.StrictMode>*/
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+/* ToDo -- CallBack and Promise explained
+setTimeout(()=> console.log("Bears"), 2000);
+
+console.log("Beats");
+console.log("Battlestar Galactica");
+
+
+ToDo - Promise section
+new Promise(resolve => {
+    setTimeout(()=> {
+        console.log("Bears");
+        resolve();
+    }, 2000);
+}).then(()=> {
+    console.log("Beats");
+    console.log("Battlestar Galactica");
+});
+
+
+
+//.then wait for result to be resolved (i.e wait for resolve())
+new Promise((resolve, reject) => {
+    return reject(new Error("No bears"));
+
+    setTimeout(()=> {
+        console.log("Bears");
+        resolve("Beats Battlestar Galactica");
+    }, 2000);
+}).then(quote => {
+    console.log(quote);
+}).catch( error => console.log(error));
+ */
